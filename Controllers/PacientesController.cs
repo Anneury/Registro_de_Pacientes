d77e201d3312aded46383966c35a594b38736cde
Anneury Sosa <anneurysosa123@gmail.com>
2021-10-05 16:55:56 -0400

    Add project files.

---
+++ Controllers/PacientesController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Registro_de_Pacientes;
+
+namespace Registro_de_Pacientes.Controllers
+{
+    public class PacientesController : Controller
+    {
+        private DatosEntities db = new DatosEntities();
+
+        // GET: Pacientes
+        //public ActionResult Index()
+        //{
+        //    return View(db.Pacientes.ToList());
+        //}
+        [HttpPost]
+        public string Index(FormCollection fc, string searchString)
+        {
+            return "<h3> From [HttpPost]Index: " + searchString + "</h3>";
+        }
+
+        public ActionResult Index(string searchString)
+        {
+            
+            var pacientes = from m in db.Pacientes
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                pacientes = pacientes.Where(s => s.Nombre.Contains(searchString));
+            }
+
+            return View(pacientes);
+        }
+
+        // GET: Pacientes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pacientes pacientes = db.Pacientes.Find(id);
+            if (pacientes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pacientes);
+        }
+
+        // GET: Pacientes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pacientes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Cedula,Nombre,Apellido,Edad,Correo,MotivoConsulta,Observaciones,Fecha")] Pacientes pacientes)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pacientes.Add(pacientes);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pacientes);
+        }
+
+        // GET: Pacientes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pacientes pacientes = db.Pacientes.Find(id);
+            if (pacientes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pacientes);
+        }
+
+        // POST: Pacientes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Cedula,Nombre,Apellido,Edad,Correo,MotivoConsulta,Observaciones,Fecha")] Pacientes pacientes)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pacientes).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pacientes);
+        }
+
+        // GET: Pacientes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pacientes pacientes = db.Pacientes.Find(id);
+            if (pacientes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pacientes);
+        }
+
+        // POST: Pacientes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Pacientes pacientes = db.Pacientes.Find(id);
+            db.Pacientes.Remove(pacientes);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Registro_de_Pacientes.MvcApplication" Language="C#" %>
